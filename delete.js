db68b47676ec399ffab1851addc1d4616a78f561
James D.A. Smith <jamiesmith66@gmail.com>
2019-02-28 11:21:36 -0500

    add update and delete functions

--- delete.js
+++ delete.js
@@ -3,12 +3,12 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: "teams",
+    TableName: "seasons",
     // 'Key' defines the partition key and sort key of the item to be removed
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      teamId: event.pathParameters.id
+      seasonId: event.pathParameters.id
     }
   };
 
--- update.js
+++ update.js
@@ -4,19 +4,18 @@ import { success, failure } from "./libs/response-lib";
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
-    TableName: "teams",
+    TableName: "seasons",
     // 'Key' defines the partition key and sort key of the item to be updated
     // - 'userId': Identity Pool identity id of the authenticated user
     // - 'noteId': path parameter
     Key: {
-      teamId: event.pathParameters.id
+      seasonId: event.pathParameters.id
     },
     // 'UpdateExpression' defines the attributes to be updated
     // 'ExpressionAttributeValues' defines the value in the update expression
-    UpdateExpression: "SET teamName = :teamName, seasonId = :seasonId",
+    UpdateExpression: "SET seasonName = :seasonName",
     ExpressionAttributeValues: {
-      ":teamName": data.teamName || null,
-      ":seasonId": data.seasonId || null
+      ":seasonName": data.seasonName || null
     },
     // 'ReturnValues' specifies if and how to return the item's attributes,
     // where ALL_NEW returns all attributes of the item after the update; you
