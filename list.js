0ea021477e00ed2881da5aee6a0080fc8bfc99fc
James D.A. Smith <jamiesmith66@gmail.com>
2019-03-01 19:37:27 -0500

    Add list function

--- list.js
+++ list.js
@@ -3,15 +3,13 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context) {
   const params = {
-    TableName: "seasons",
-    Key: {
-      seasonId: event.pathParameters.id
-    }
+    TableName: "seasons"
   };
 
   try {
-    const result = await dynamoDbLib.call("delete", params);
-    return success({ status: true });
+    const result = await dynamoDbLib.call("scan", params);
+    // Return the matching list of items in response body
+    return success(result.Items);
   } catch (e) {
     return failure({ status: false });
   }
